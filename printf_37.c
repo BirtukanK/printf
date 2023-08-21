069e425a2532bbc68ae21ddefe714a6a9d3bc2e4
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-08-21 11:54:15 -0700

    task 0

--- printf_37.c
+++ printf_37.c
@@ -1,29 +1,10 @@
 #include "main.h"
 /**
- * printf_string - print a string.
- * @val: argumen t.
- * Return: the length of the string.
+ * printf_37 - prints the char 37.
+ * Return: 1.
  */
-
-int printf_string(va_list val)
+int printf_37(void)
 {
-	char *s;
-	int i, len;
-
-	s = va_arg(val, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
+	_putchar(37);
+	return (1);
 }
